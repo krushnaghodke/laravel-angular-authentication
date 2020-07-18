2af5936ec2e0738381b6e58cdc28e0071334b425
Krishna Ghodke <krishna.ghodke@quest-global.com>
2020-07-18 18:17:39 +0530

    Profile and canActivate

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../Services/auth.service";
+import {Router} from "@angular/router";
+import {TokenService} from "../../Services/token.service";
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +13,20 @@ export class NavbarComponent implements OnInit {
   public loggedIn: boolean;
 
   constructor(
-    private Auth: AuthService
+    private Auth: AuthService,
+    private router: Router,
+    private token: TokenService
   ) { }
 
   ngOnInit(): void {
     this.Auth.authStatus.subscribe(value => this.loggedIn = value);
   }
 
+  logout(event: MouseEvent) {
+    event.preventDefault();
+
+    this.Auth.changeAuthStatus(false);
+    this.token.remove();
+    this.router.navigateByUrl('/login');
+  }
 }
