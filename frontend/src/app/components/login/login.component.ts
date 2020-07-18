93066826beb66b53bf36254758020c86e46920df
Krishna Ghodke <krishna.ghodke@quest-global.com>
2020-07-18 17:51:31 +0530

    Login and logout link

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {JarwisService} from "../../Services/jarwis.service";
 import {TokenService} from "../../Services/token.service";
+import {Router} from "@angular/router";
+import {AuthService} from "../../Services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -18,7 +20,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private Jarwis: JarwisService,
-    private Token: TokenService
+    private Token: TokenService,
+    private router: Router,
+    private Auth: AuthService
   ) { }
 
   ngOnInit(): void { }
@@ -32,6 +36,8 @@ export class LoginComponent implements OnInit {
 
   handleResponse(data) {
     this.Token.handle(data.access_token);
+    this.Auth.changeAuthStatus(true);
+    this.router.navigateByUrl('/profile');
   }
 
   handleError(error){
